0a90721dcc88f335bcc189d2c87439fe986a8a11
Yoohwan-Roh <89440787+Yoohwan-Roh@users.noreply.github.com>
2021-10-11 13:56:00 +0900

    Add files via upload

--- common.js
+++ common.js
@@ -1,15 +1,15 @@
 // gnb 마우스 호버
-// $(".smallNav").hide();
-// $(".bigNav > li").on('mouseenter', function() {
-//   $(".bigNav > li").not($(this)).children('a').css({ color: "#fff" })
-//   $(this).children('a').css({ color: "#000"})
-//   $(".bigNav > li").not($(this)).children('.smallNav').slideUp();
-//   $(this).children('.smallNav').slideDown(500);
-// })
-// .on('mouseleave', function() {
-//     $(this).children('a').css({ color: "#fff"})
-//     $(this).children('.smallNav').slideUp();
-// })
+$(".smallNav").hide();
+$(".bigNav > li").on('mouseenter', function() {
+  $(".bigNav > li").not($(this)).children('a').css({ color: "#fff" })
+  $(this).children('a').css({ color: "#000"})
+  $(".bigNav > li").not($(this)).children('.smallNav').slideUp();
+  $(this).children('.smallNav').slideDown(500);
+})
+.on('mouseleave', function() {
+    $(this).children('a').css({ color: "#fff"})
+    $(this).children('.smallNav').slideUp();
+})
 var swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         centeredSlides: true,
